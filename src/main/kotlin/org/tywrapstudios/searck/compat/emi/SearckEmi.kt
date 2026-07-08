e8aa8ca76130541256f1c683c561ce45fbbd6809
ItsTiazzz <163735662+ItsTiazzz@users.noreply.github.com>
2026-07-08 21:13:53 +0200

    Fix EMI plugin entrypoint warn

--- src/main/kotlin/org/tywrapstudios/searck/compat/emi/SearckEmi.kt
+++ src/main/kotlin/org/tywrapstudios/searck/compat/emi/SearckEmi.kt
@@ -1,5 +1,13 @@
-package org.tywrapstudios.searck.compat.emi
+//? <1.21.11 {
+/*package org.tywrapstudios.searck.compat.emi
+
+import dev.emi.emi.api.EmiPlugin
+import dev.emi.emi.api.EmiRegistry
 
 // TODO: After porting 1.21.x is ready
-object SearckEmi {
+object SearckEmi : EmiPlugin {
+    override fun register(p0: EmiRegistry) {
+
+    }
 }
+*///?}
